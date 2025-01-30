fd03bb47b98390361ec50ea84ad50890859429e2
Jon Pascal Miklavcic <jon.pascal.yt@gmail.com>
2025-01-30 20:12:46 +0100

    Added results of 1-11 vertices complete search

--- Implementation/experimentation.py
+++ Implementation/experimentation.py
@@ -2,6 +2,7 @@ import os
 import random
 import pickle
 from itertools import islice
+
 from sage.all import *
 from sage.graphs.graph_generators import graphs
 
@@ -10,6 +11,9 @@ from Implementation.graph_generating import *
 
 
 
+########################################################################################################
+##       Code for searching the highest PCN for graphs with up to 11 vertices using nauty_geng        ##
+########################################################################################################
 
 def save_state(filename, highest_value, best_graphs, start_iteration):
     """
@@ -36,7 +40,7 @@ def load_state(filename):
 
 
 
-def main(m=100, graph_number=1, state_file="graph_state.pkl"): 
+def complete_search(m=100, state_file="graph_state.pkl"): 
     highest_value = 0
     best_graphs = []
 
@@ -44,7 +48,7 @@ def main(m=100, graph_number=1, state_file="graph_state.pkl"):
         state = load_state(state_file)
         highest_value = state["highest_value"]
         best_graphs = state["best_graphs"]
-        start_iteration = state["start_iteration"]
+        start_iteration = state["start_iteration"] 
         print(f"Resuming from iteration {start_iteration} with max coloring value {highest_value}.")
     except FileNotFoundError:
         start_iteration = 1
@@ -54,21 +58,18 @@ def main(m=100, graph_number=1, state_file="graph_state.pkl"):
 
         print(f"Starting iteration: {n}")
 
-        for (i, G) in islice(enumerate(graphs.nauty_geng(f"{n} -c")), graph_number, None): 
+        for (i, G) in enumerate(graphs.nauty_geng(f"{n} -c")): 
 
             print(f"Looking at graph {i} with {n} vertices")
 
             if G.is_planar() and max(G.degree(v) for v in G.vertices()) <= 3: 
-                packing_coloring_number = barvanje(G)
-
+                packing_coloring_number = packing_coloring(G)
                 if packing_coloring_number > highest_value:
-
                     print(f"Graph has the PCN: {packing_coloring_number}")
                     highest_value = packing_coloring_number
                     best_graphs = [G]
 
                 elif packing_coloring_number == highest_value:
-
                     print(f"Graph has the PCN: {packing_coloring_number}")
                     best_graphs.append(G)
 
@@ -77,23 +78,12 @@ def main(m=100, graph_number=1, state_file="graph_state.pkl"):
         save_state(state_file, highest_value, best_graphs, n)
         print(f"State saved at iteration {n}.")
     
-    # Create the "Results" folder if it doesn't exist
-    results_folder = "Results"
-    if not os.path.exists(results_folder):
-        os.makedirs(results_folder)
-
-    # Save the best graphs into the "Results" folder
-    for idx, best_graph in enumerate(best_graphs, start=1):
-        filename = f"{results_folder}/best_graph_{idx}_color_{highest_value}.graph.sobj"
-        best_graph.save(filename)
-        print(f"Graph #{idx} saved as {filename}")
 
 
 
 
 def display_best_graphs(state_file="graph_state.pkl"):
-    # LOOKING AT THE BEST GRAPHS
-    state = load_state("graph_state.pkl")
+    state = load_state(state_file)
 
     # Extract relevant data
     highest_value = state["highest_value"]
